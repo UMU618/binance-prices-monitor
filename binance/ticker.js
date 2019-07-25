9443c4265fa3e18838071d4af61e587f7d8e731b
UMU618 <umu618@hotmail.com>
2019-07-25 10:43:36 +0800

    +

---
+++ binance/ticker.js
@@ -0,0 +1,31 @@
+/**
+ * @author UMU618 <umu618@hotmail.com>
+ * @copyright MEET.ONE 2019
+ * @description Use block-always-using-brace npm-coding-style.
+ */
+
+'use strict'
+
+const conf = require('../conf')
+const assert = require('assert')
+const fetch = require('node-fetch')
+
+module.exports = {
+  get24hr: function (symbol) {
+    assert(symbol, 'no symbol')
+    return fetch(conf.baseUrl + '/api/v1/ticker/24hr?symbol=' + symbol)
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+        else {
+          console.log('API failed, status = ' + res.status)
+          return null
+        }
+      }
+      , (e) => {
+        console.error('Call API failed, error: ' + e)
+        return null
+      })
+  }
+};
---
+++ index.js
@@ -0,0 +1,67 @@
+/**
+ * @author UMU618 <umu618@hotmail.com>
+ * @copyright MEET.ONE 2019
+ * @description Use block-always-using-brace npm-coding-style.
+ */
+
+'use strict'
+
+const conf = require('./conf')
+const ticker = require('./binance/ticker')
+const alerts = require('./utils/alerts')
+
+let lastPrice = 0
+
+setInterval(() => {
+  ticker
+    .get24hr(conf.symbol)
+    .then((jo) => {
+      if (jo && jo[0]) {
+        compare24hr(jo[0].openPrice, jo[0].lastPrice)
+        if (lastPrice != 0) {
+          compare(lastPrice, jo[0].lastPrice)
+        }
+        lastPrice = jo[0].lastPrice
+      }
+    })
+}, conf.interval)
+
+function compare24hr(open, close) {
+  if (open < close) {
+    if ((close - open) / open >= conf.last24hPrecent) {
+      if (lastPrice == 0) {
+        alerts.sendDingtalk(conf.dingtalkToken, conf.baseAsset + ' price increase to ' + close)
+      }
+      else {
+        if (lastPrice < close) {
+          alerts.sendDingtalk(conf.dingtalkToken, conf.baseAsset + ' price increase to ' + close)
+        }
+      }
+    }
+  }
+  else {
+    if ((open - close) / open >= conf.last24hPrecent) {
+      if (lastPrice == 0) {
+        alerts.sendDingtalk(conf.dingtalkToken, conf.baseAsset + ' price fall to ' + close,)
+      }
+      else {
+        if (lastPrice > close) {
+          alerts.sendDingtalk(conf.dingtalkToken, conf.baseAsset + ' price fall to ' + close)
+        }
+      }
+    }
+  }
+}
+
+function compare(oldPrice, newPrice) {
+  if (oldPrice < newPrice) {
+    // increasing
+    if ((newPrice - oldPrice) / oldPrice >= conf.diffPrecent) {
+      alerts.sendDingtalk(conf.dingtalkToken, conf.baseAsset + ' price increase to ' + newPrice)
+    }
+  } else {
+    if ((oldPrice - newPrice) / oldPrice >= conf.diffPrecent) {
+      alerts.sendDingtalk(conf.dingtalkToken, conf.baseAsset + ' price fall to ' + newPrice)
+    }
+  }
+}
